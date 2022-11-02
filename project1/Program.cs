153b4753679d14b4f8a997919bd3b533e378d844
Alisa <alisenok5@mail.ru>
2022-11-02 22:57:26 +0300

    feat: solve task 2

--- project1/Program.cs
+++ project1/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.WriteLine("Введите первое целое число");
+int a = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int b = Int32.Parse(Console.ReadLine());
+
+int max = a;
+
+if (a < b) {
+    max = b;
+}
+
+Console.WriteLine("Max = " + max);
