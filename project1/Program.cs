1b708490e885a587f18756022aef9c2de105659c
Alisa <alisenok5@mail.ru>
2022-11-02 23:07:28 +0300

    feat: solve task 4

--- project1/Program.cs
+++ project1/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿Console.WriteLine("Введите первое число из трех");
+int a = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число из трех");
+int b = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число из трех");
+int c = Int32.Parse(Console.ReadLine());
+
+int max = a;
+
+if (max < b) {
+    max = b;
+}
+if (max < c) {
+    max = c;
+}
+
+Console.WriteLine("Max = " + max);
