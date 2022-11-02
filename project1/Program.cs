87db16291aeaaa4db39a90bc826b771c6168764f
Alisa <alisenok5@mail.ru>
2022-11-02 23:44:11 +0300

    feat: solve task 8

--- project1/Program.cs
+++ project1/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.WriteLine("Введите целое число");
+int number = Int32.Parse(Console.ReadLine());
+
+int i = 1;
+
+while (i <= number) {
+    if (i % 2 == 0) {
+        if (i == number) {
+            Console.WriteLine(i);
+        } else {
+            Console.Write(i + ", ");
+        }
+    }
+    i++;
+}
