c13b1826a59da0774c14fada4196859c87433fc5
Alisa <alisenok5@mail.ru>
2022-11-03 00:11:27 +0300

    fix: use Convert.ToInt32()

--- project1/Program.cs
+++ project1/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Введите целое число");
-int number = Int32.Parse(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
 string result = "нет";
 
 if (number % 2 == 0) {
