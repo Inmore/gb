fb83e6367877c219979bf08cf2ad5ba91d8e5c06
Alisa <alisenok5@mail.ru>
2022-11-02 23:18:54 +0300

    feat: solve task 6

--- project1/Program.cs
+++ project1/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.WriteLine("Введите целое число");
+int number = Int32.Parse(Console.ReadLine());
+string result = "нет";
+
+if (number % 2 == 0) {
+    result = "да";
+}
+
+Console.WriteLine(result);
