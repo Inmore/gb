7515a8478fd17824b24517157038aedd2dcf3e49
Alisa <alisenok5@mail.ru>
2022-11-03 00:20:57 +0300

    fix: fix some mistakes

--- project1/Program.cs
+++ project1/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.WriteLine("Введите целое число");
-int number = Int32.Parse(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
 
 int i = 1;
 
 while (i <= number) {
     if (i % 2 == 0) {
-        if (i == number) {
+        if (i == number || i == number - 1) {
             Console.WriteLine(i);
         } else {
             Console.Write(i + ", ");
