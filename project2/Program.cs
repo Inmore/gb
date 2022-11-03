affaf28ffa78f8eed67f3c459a0c373229ee43f1
Alisa <alisenok5@mail.ru>
2022-11-04 00:36:20 +0300

    feat: solve task13

--- project2/Program.cs
+++ project2/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+int digit = 0;
+
+if (number / 100 > 0)
+{
+    if (number < 1000) {
+        digit = number / 100;
+    } else {
+        digit = (number / 100) % 10;
+    }
+    Console.WriteLine(digit);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
