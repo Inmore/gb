26e04e035e79ce0b581041ed720fe71192338ec8
Alisa <alisenok5@mail.ru>
2022-11-04 15:33:00 +0300

    refactor code

--- project2/Program.cs
+++ project2/Program.cs
@@ -1,5 +1,7 @@
 ﻿Console.WriteLine("Введите целое трехзначное число");
 int number = Convert.ToInt32(Console.ReadLine());
+
 int result = 0;
 result = (number / 10) % 10;
+
 Console.WriteLine(result);
