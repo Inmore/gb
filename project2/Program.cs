892e2cc22c40d204be411ec2fa8477a3508c10d0
Alisa <alisenok5@mail.ru>
2022-11-04 11:31:25 +0300

    feat: solve task15

--- project2/Program.cs
+++ project2/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.WriteLine("Введите номер дня недели от 1 до 7");
+int dayNum = Convert.ToInt32(Console.ReadLine());
+string message = "";
+
+if (dayNum > 0 && dayNum <= 7)
+{
+    if (dayNum == 6 || dayNum == 7)
+    {
+        message = "да";
+    } else {
+        message = "нет";
+    }
+    Console.WriteLine(message);
+}
+
