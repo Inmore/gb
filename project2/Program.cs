fc343156261b007ca473635dd352d4af73b75781
Alisa <alisenok5@mail.ru>
2022-11-04 15:30:06 +0300

    solve task10

--- project2/Program.cs
+++ project2/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿Console.WriteLine("Введите целое трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+result = (number / 10) % 10;
+Console.WriteLine(result);
