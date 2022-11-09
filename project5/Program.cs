cf4dbb7598dc1399b23666fb1091bb4ab7468175
Alisa <alisenok5@mail.ru>
2022-11-09 22:34:50 +0300

    feat: solve task 34

--- project5/Program.cs
+++ project5/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+int[] array = new int[4];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+}
+
+int CountEven(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) {
+            count++;
+        }
+    }
+    return count;
+}
+
