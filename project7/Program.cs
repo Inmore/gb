fc2383d8e14463e9410690792a2891f17de96721
Alisa <alisenok5@mail.ru>
2022-11-12 14:00:22 +0300

    feat: solve task 52

--- project7/Program.cs
+++ project7/Program.cs
@@ -76,3 +76,31 @@ string InArray(int num, int[,] array) {
     return result;
 }
 // Console.WriteLine(InArray(17, array_50));
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов 
+// в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+Console.WriteLine(ColumnsAverage(array_50));
+string ColumnsAverage(int[,] array) {
+    string result = "";
+ 
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        double sum = 0.0;
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            sum = sum + array[j, i];
+        }
+ 
+        double average = Math.Round(sum / array.GetLength(0), 1);
+ 
+        result = result + $"{average}";
+        if (i != array.GetLength(1) - 1) result = result + "; ";
+    }
+ 
+    return "Среднее арифметическое каждого столбца: " + result + ".";
+}
