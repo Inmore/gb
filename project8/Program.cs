982897a7853da3e8991c455fb1f7f1904ed312c9
Alisa <alisenok5@mail.ru>
2022-11-14 22:01:00 +0300

    feat: solve task 56

--- project8/Program.cs
+++ project8/Program.cs
@@ -64,3 +64,41 @@ int[,] SortLineInt2DArray(int[,] array) {
     }
     return array;
 }
+
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет 
+// находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей 
+// суммой элементов: 1 строка
+ 
+// Задаем массив
+int[,] array_56 = new int[,] {{1, 4, 7, 2}, {5, 9, 2, 3}, {8, 4, 2, 4}, {5, 2, 6, 7}};
+Console.WriteLine(MinSumLine(array_56));
+string MinSumLine(int[,] array) {
+    int temp = 0;
+    int line_number = 1;
+ 
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+ 
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum = sum + array[i, j];
+        }
+ 
+        if (i == 0) {
+            temp = sum;
+        }
+ 
+        if (sum < temp) {
+            temp = sum;
+            line_number = i + 1;
+        }
+    }
+    return line_number + " строка";
+}
