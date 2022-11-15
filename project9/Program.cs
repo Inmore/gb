9f47604e8d5f03af4b6bcec31027822f269d1783
Alisa <alisenok5@mail.ru>
2022-11-15 23:00:24 +0300

    feat: solve task 66

--- project9/Program.cs
+++ project9/Program.cs
@@ -18,3 +18,31 @@ string PrintNumbers(int n)
 
     return result = n + comma + PrintNumbers(n - 1);
 }
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Console.WriteLine("Введите натуральное число");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите натуральное число");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(NumbersSum(m, n));
+
+int NumbersSum(int m, int n) {
+    if (m < 1) m = 1;
+    if (n < 1) n = 1;
+
+    if (m == n) return m;
+
+    int temp = 0;
+
+    if (m > n) {
+        temp = m;
+        m = n;
+        n = temp;
+    }
+
+    return n + NumbersSum(m, n - 1);
+}
