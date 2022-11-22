7ecab815b4e06c53433aa500e90b26a598152235
Alisa <alisenok5@mail.ru>
2022-11-22 22:52:46 +0300

    feat: solve final task

--- finalproject/Program.cs
+++ finalproject/Program.cs
@@ -1,1 +1,57 @@
-﻿
+﻿string[] FilterArray()
+{
+    Console.WriteLine("Введите количество элементов массива");
+    int m = Convert.ToInt32(Console.ReadLine());
+ 
+    if (m > 0)
+    {
+        string[] array = new string[m];
+ 
+        Console.WriteLine($"Введите поочередно элементы массива");
+ 
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = Console.ReadLine();
+        }
+ 
+        int count = 0;
+ 
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= 3) {
+                count++;
+            } 
+        }
+ 
+        string[] result = new string[count];
+        int k = 0;
+ 
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= 3) {
+                result[k] = array[i];
+                k++;
+            } 
+        }
+ 
+        return result;
+    } else {
+        throw new ArgumentException("Ожидается натуральное число");
+    }
+ 
+}
+string[] array = FilterArray();
+PrintStringArray(array);
+void PrintStringArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        string result = "";
+ 
+        result = $"{array[i]}";
+        Console.Write(result);
+ 
+        if (i != array.Length - 1) Console.Write(", "); 
+    }
+    Console.WriteLine("");
+}
