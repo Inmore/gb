96b3262016d3842f8b22e579be3593bb71e96b76
Alisa <alisenok5@mail.ru>
2022-12-06 13:12:44 +0300

    feat: solve task 2

---
+++ python/homework3/task2.py
@@ -0,0 +1,18 @@
+import math
+
+
+# 2.Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+#
+# *Пример:*
+#
+# - [2, 3, 4, 5, 6] => [12, 15, 16];
+# - [2, 3, 5, 6] => [12, 15]
+def pair_multiply(input_list):
+    count = len(input_list)
+    result = []
+    for item in range(math.ceil(count / 2)):
+        result.append(input_list[item] * list(reversed(input_list))[item])
+    return result
+
+
+print(pair_multiply([2, 3, 4, 5, 6]))
